605f5cb441b8bb775b64bd5820334a069980b73b
samratghosh291 <samratghosh490@gmail.com>
2024-01-10 15:43:28 +0530

    4th commit

---
+++ Modifier_Exercise.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract PausableToken {
+    address public owner;
+    bool public paused;
+    mapping(address => uint) public balances;
+
+    constructor() {
+        owner = msg.sender;
+        paused = false;
+        balances[owner] = 1000;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "YOU ARE NOT THE OWNER!");
+        _;
+    }
+
+    modifier notPaused() {
+        require(!paused, "THE CONTRACT IS PAUSED");
+        _;
+    }
+
+    function pause() public onlyOwner {
+        paused = true;
+    }
+
+    function unpause() public onlyOwner {
+        paused = false;
+    }
+
+    function transfer(address to, uint amount) public notPaused {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
